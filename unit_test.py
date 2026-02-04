fad4ec7c5f312ce2ae3b1116bd078923075677cf
LukeZavierWieder <lzw5614@psu.edu>
2026-02-04 15:15:19 -0500

    Renamed File and added normal add_book unit test
    File renamed from assignment-2 to assignment_2
    Created normal test case for add_book

--- unit_test.py
+++ unit_test.py
@@ -2,3 +2,29 @@
 Testing personal library code
 Tester: Luke Wieder
 """
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+
+from assignment_2 import (
+    add_book,
+    remove_book,
+    update_book,
+    list_all_books,
+    search,
+    show_stats
+)
+
+# Test class
+class TestPersonalLibrary(unittest.TestCase):
+    # Test case for add book function
+    def test_add_book_normal_case(self):
+        library = []
+        with patch("builtins.input", return_value = "Dungeon Crawler Carl"):
+            add_book(library)
+        self.assertEqual(library, ["Dungeon Crawler Carl"])
+
+
+if __name__ == "__main__":
+    unittest.main()
